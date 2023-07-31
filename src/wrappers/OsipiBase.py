f13fc11d946b60354de0b2fa7c8b0b977821ac29
Eric Peterson <etpeters@sri.com>
2023-07-30 21:54:10 -0700

    WIP: draft of base class for fitting wrapper

---
+++ src/wrappers/OsipiBase.py
@@ -0,0 +1,38 @@
+
+
+
+class ivim_fit_osipi:
+    """The base class for OSIPI IVIM fitting"""
+
+    def fit_osipi(self, data=None, bvalues=None, initial_guess=None, bounds=None, **kwargs):
+        """Fits the data with the bvalues
+        Returns [S0, f, D, D*]
+        """
+        pass
+
+    def accepted_dimensions_osipi(self):
+        """The array of accepted dimensions
+        e.g.
+        (1D,   2D,   3D,    4D,    5D,    6D)
+        (True, True, False, False, False, False)
+        """
+        return (False,) * 6
+
+    def accepts_dimension_osipi(self, dim):
+        """Query if the selection dimension is fittable"""
+        accepted = self.accepted_dimensions()
+        if dim < 0 or dim > len(accepted):
+            return False
+        return accepted[dim]
+
+    def guess_required_osipi():
+        """Does it require an initial guess?"""
+        return False
+
+    def bounds_required_osipi():
+        """Does it require bounds?"""
+        return False
+
+    def author_osipi():
+        """Author identification"""
+        return ''
