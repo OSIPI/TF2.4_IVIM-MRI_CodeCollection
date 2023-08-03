9a2f412831f6be1a90670a7f31bdf4365aba979a
IvanARashid <ivan52x53@gmail.com>
2023-08-03 16:23:12 +0200

    Continued work on conformance. Added algorithm defining attributes to the algorithm-specific class. Added simple RMSE calculation for single parameter set to OsipiBase.

--- src/standardized/ETP_SRI_LinearFitting.py
+++ src/standardized/ETP_SRI_LinearFitting.py
@@ -8,9 +8,50 @@ class ETP_SRI_LinearFitting(OsipiBase):
     Implementation and execution of the submitted algorithm
     """
     
-    def ivim_fit(self, signals, b_bvalues):
-        ETP_object = LinearFit(self.thresholds[0])
+    # I'm thinking that we define default attributes for each submission like this
+    # And in __init__, we can call the OsipiBase control functions to check whether
+    # the user inputs fulfil the requirements
+    
+    # Some basic stuff that identifies the algorithm
+    author = "Eric T. Peterson, SRI"
+    algorithm_type = "Linear fit"
+    return_parameters = "f, D*, D"
+    units = "seconds per milli metre squared"
+    
+    # Algorithm requirements
+    b_values_required = 3
+    thresholds_required = [1,1] # Interval from 1 to 1, in case submissions allow a custom number of thresholds
+    bounds_required = False
+    accepted_dimensions = 1
+    
+    def __init__(self, thresholds, weighting=None, stats=False):
+        """
+            Everything this method requires should be implemented here.
+            Number of segmentation thresholds, bounds, etc.
+            
+            Our OsipiBase object could contain functions compare the inputs with
+            the requirements.
+        """
+        self.thresholds = thresholds
+        # Could be a good idea to have all the submission-specfic variable be 
+        # defined with initials?
+        self.ETP_weighting = weighting
         
+        # Check the inputs
+        self.check_thresholds_required_osipi()
+        
+    
+    def ivim_fit(self, signals, b_values):
+        """
+            We may want this function to be as simple as
+            data and b-values in -> parameter estimates out.
+            Everything else such as segmentation thresholds, bounds, etc. should
+            be object attributes.
+            
+            This makes the execution of submissions easy and predictable.
+            All execution stepts requires should be performed here.
+        """
+        ETP_object = LinearFit(self.thresholds[0])
         f, D, Dstar = ETP_object.ivim_fit(b_values, signals)
         
         
@@ -27,6 +68,6 @@ def ivim_model(b, S0=1, f=0.1, Dstar=0.03, D=0.001):
 
 signals = ivim_model(b_values)
 
-model = ETP_SRI_LinearFitting()
-model.thresholds = [200]
-results = model.ivim_fit(b_values, signals)
+model = ETP_SRI_LinearFitting([200])
+results = model.ivim_fit(signals, b_values)
+test = model.simple_bias_and_RMSE_test(SNR=20, b_values=b_values, f=0.1, Dstar=0.03, D=0.001, noise_realizations=10)
