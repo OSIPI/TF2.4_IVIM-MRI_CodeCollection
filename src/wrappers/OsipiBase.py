6cf5360aa62e848386662399eb3aeb280f986aa3
IvanARashid <ivan52x53@gmail.com>
2023-08-03 16:23:12 +0200

    Continued work on conformance. Added algorithm defining attributes to the algorithm-specific class. Added simple RMSE calculation for single parameter set to OsipiBase.

--- src/wrappers/OsipiBase.py
+++ src/wrappers/OsipiBase.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+from scipy.stats import norm
 
 
 class OsipiBase:
@@ -29,22 +30,57 @@ class OsipiBase:
             return False
         return accepted[dim]
     
-    def thresholds_required_osipi():
+    def check_thresholds_required_osipi(self):
         """How many segmentation thresholds does it require?"""
-        return 0
+        if (len(self.thresholds) < self.thresholds_required[0]) or (len(self.thresholds) > self.thresholds_required[1]):
+            print("Conformance error: Number of thresholds.")
+            return False
+        return True
 
-    def guess_required_osipi():
+    def check_guess_required_osipi():
         """Does it require an initial guess?"""
         return False
 
-    def bounds_required_osipi():
+    def check_bounds_required_osipi():
         """Does it require bounds?"""
         return False
+    
+    def check_b_values_required_osipi():
+        """Minimum number of b-values required"""
+        pass
 
     def author_osipi():
         """Author identification"""
         return ''
     
-    def simple_test():
-        pass
+    def simple_bias_and_RMSE_test(self, SNR, b_values, f, Dstar, D, noise_realizations=100):
+        # Generate signal
+        b_values = np.asarray(b_values)
+        signals = f*np.exp(-b_values*Dstar) + (1-f)*np.exp(-b_values*D)
+        
+        f_estimates = np.zeros(noise_realizations)
+        Dstar_estimates = np.zeros(noise_realizations)
+        D_estimates = np.zeros(noise_realizations)
+        for i in range(noise_realizations):
+            # Add some noise
+            sigma = signals[0]/SNR
+            noised_signal = np.array([norm.rvs(signal, sigma) for signal in signals])
+            
+            # Perform fit with the noised signal
+            f_estimates[i], Dstar_estimates[i], D_estimates[i] = self.ivim_fit(noised_signal, b_values)
+            
+        # Calculate bias
+        f_bias = np.mean(f_estimates) - f
+        Dstar_bias = np.mean(Dstar_estimates) - Dstar
+        D_bias = np.mean(D_estimates) - D
+            
+        # Calculate RMSE
+        f_RMSE = np.sqrt(np.var(f_estimates) + f_bias**2)
+        Dstar_RMSE = np.sqrt(np.var(Dstar_estimates) + Dstar_bias**2)
+        D_RMSE = np.sqrt(np.var(D_estimates) + D_bias**2)
+            
+        print(f"f bias:\t{f_bias}\nf RMSE:\t{f_RMSE}")
+        print(f"Dstar bias:\t{Dstar_bias}\nDstar RMSE:\t{Dstar_RMSE}")
+        print(f"D bias:\t{D_bias}\nD RMSE:\t{D_RMSE}")
+            
     
