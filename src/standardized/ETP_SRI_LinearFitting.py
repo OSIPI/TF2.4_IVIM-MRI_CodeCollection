3f5634f0ab8790d12197cfa30cc617dbe04c6321
IvanARashid <ivan52x53@gmail.com>
2023-08-31 14:43:52 +0200

    Implemented basic osipi_fit method to the base class
    My thinking is that this is the default function for fitting. It basically passes arguements to the ivim_fit method of standardized version of the algorithm submission. But before a fit is performed, checks should be made to make sure algorithm requirements are fulfilled. This is yet to be implemented

--- src/standardized/ETP_SRI_LinearFitting.py
+++ src/standardized/ETP_SRI_LinearFitting.py
@@ -75,13 +75,15 @@ class ETP_SRI_LinearFitting(OsipiBase):
     
 
 ## Simple test code... 
-#bvalues = np.array([0, 200, 500, 800])
+bvalues = np.array([0, 200, 500, 800])
 
-#def ivim_model(b, S0=1, f=0.1, Dstar=0.03, D=0.001):
-    #return S0*(f*np.exp(-b*Dstar) + (1-f)*np.exp(-b*D))
+def ivim_model(b, S0=1, f=0.1, Dstar=0.03, D=0.001):
+    return S0*(f*np.exp(-b*Dstar) + (1-f)*np.exp(-b*D))
 
-#signals = ivim_model(bvalues)
+signals = ivim_model(bvalues)
 
-#model = ETP_SRI_LinearFitting()
+model = ETP_SRI_LinearFitting(thresholds=[200])
 #results = model.ivim_fit(signals, bvalues)
+results = model.osipi_fit(signals, bvalues)
+print(results)
 #test = model.osipi_simple_bias_and_RMSE_test(SNR=20, bvalues=bvalues, f=0.1, Dstar=0.03, D=0.001, noise_realizations=10)
