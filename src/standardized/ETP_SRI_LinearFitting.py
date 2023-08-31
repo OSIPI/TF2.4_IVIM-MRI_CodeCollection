7425824d4b971959b68a7afeed5f70585b9f1014
IvanARashid <ivan52x53@gmail.com>
2023-08-31 15:56:51 +0200

    Moved the simple test run to separate file

--- src/standardized/ETP_SRI_LinearFitting.py
+++ src/standardized/ETP_SRI_LinearFitting.py
@@ -73,17 +73,3 @@ class ETP_SRI_LinearFitting(OsipiBase):
             f, D, Dstar = ETP_object.ivim_fit(bvalues, signals)
             return f, Dstar, D
     
-
-## Simple test code... 
-bvalues = np.array([0, 200, 500, 800])
-
-def ivim_model(b, S0=1, f=0.1, Dstar=0.03, D=0.001):
-    return S0*(f*np.exp(-b*Dstar) + (1-f)*np.exp(-b*D))
-
-signals = ivim_model(bvalues)
-
-model = ETP_SRI_LinearFitting(thresholds=[200])
-#results = model.ivim_fit(signals, bvalues)
-results = model.osipi_fit(signals, bvalues)
-print(results)
-#test = model.osipi_simple_bias_and_RMSE_test(SNR=20, bvalues=bvalues, f=0.1, Dstar=0.03, D=0.001, noise_realizations=10)
