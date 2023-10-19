ab747fda23a6e47e72c1b73d48be58122f61149d
Oscar Jalnefjord <oscar.jalnefjord@gu.se>
2023-10-19 16:47:12 +0200

    new try with basic matlab test

--- tests/test_matlab.py
+++ tests/test_matlab.py
@@ -1,6 +1,6 @@
 import numpy as np
-import run_matlab
+from tests.run_matlab import run
 
 def test_run_matlab():
     x = 4.0
-    np.testing.assert_array_equal(np.sqrt(x),run_matlab.run(x))
+    np.testing.assert_array_equal(np.sqrt(x),run(x))
