102e4154b2f4d8dcb6324c971a8884d286795ecf
KoenBaas <koenbaas95@gmail.com>
2023-10-26 09:14:36 +0200

    Build if statement around plotting to prevent unit test error

--- src/original/PvH_KB_NKI/DWI_Examples.py
+++ src/original/PvH_KB_NKI/DWI_Examples.py
@@ -22,6 +22,9 @@ DWI_data = np.load(file_path)
 # Specify b values that are in the data
 bvalues = [0, 15, 150, 500]
 
+# Specifiy if you want to plot the results
+plot_results = False
+
 # Generate ADC map and define the min and max b value that will be used for ADC fitting
 ADClog, b0_intercept, used_bvalues = generate_ADC_standalone(DWI_data,bvalues, bmin=150, bmax=500)
 
@@ -30,24 +33,24 @@ ADClog, b0_intercept, used_bvalues = generate_ADC_standalone(DWI_data,bvalues, b
 ADClog, perfFrac, Dstar = generate_IVIMmaps_standalone(DWI_data, bvalues, bminADC=150, bmaxADC=500, bmaxDstar=150)
 Slice = 5
 
-# Plot the ADC, f and D* maps
-fig, axes = plt.subplots(1, 3, figsize=(10, 5))
+if plot_results == True:
+    # Plot the ADC, f and D* maps
+    fig, axes = plt.subplots(1, 3, figsize=(10, 5))
 
-axes[0].imshow(ADClog[:,Slice,:], cmap='viridis', vmin=0, vmax=5)
-axes[0].set_title("ADC")
-axes[0].axis('off')
+    axes[0].imshow(ADClog[:,Slice,:], cmap='viridis', vmin=0, vmax=5)
+    axes[0].set_title("ADC")
+    axes[0].axis('off')
 
-axes[1].imshow(perfFrac[:,Slice,:], cmap='viridis', vmin=0, vmax=1)
-axes[1].set_title("f map")
-axes[1].axis('off')
+    axes[1].imshow(perfFrac[:,Slice,:], cmap='viridis', vmin=0, vmax=1)
+    axes[1].set_title("f map")
+    axes[1].axis('off')
 
-axes[2].imshow(Dstar[:,Slice,:], cmap='viridis', vmin=0, vmax=200)
-axes[2].set_title("D*")
-axes[2].axis('off')
+    axes[2].imshow(Dstar[:,Slice,:], cmap='viridis', vmin=0, vmax=200)
+    axes[2].set_title("D*")
+    axes[2].axis('off')
 
-plt.tight_layout()  # Adjust spacing between subplots
-plt.show()
-
+    plt.tight_layout()  # Adjust spacing between subplots
+    plt.show()
 
 print("finish")
 
