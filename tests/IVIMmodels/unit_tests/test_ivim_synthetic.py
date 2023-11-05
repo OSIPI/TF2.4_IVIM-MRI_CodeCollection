254f0537bd8f8ac8130e66061fdd55ffca348c95
Eric Peterson <etpeters@sri.com>
2023-11-05 15:45:01 -0800

    Improved plotting and random number generation

--- tests/IVIMmodels/unit_tests/test_ivim_synthetic.py
+++ tests/IVIMmodels/unit_tests/test_ivim_synthetic.py
@@ -16,9 +16,10 @@ from utilities.data_simulation.GenerateData import GenerateData
 @pytest.mark.slow
 def test_generated(ivim_algorithm, ivim_data, SNR, rtol, atol, fit_count, rician_noise, save_file, save_duration_file, use_prior):
     # assert save_file == "test"
-    random.seed(42)
+    rng = np.random.RandomState(42)
+    # random.seed(42)
     S0 = 1
-    gd = GenerateData()
+    gd = GenerateData(rng=rng)
     name, bvals, data = ivim_data
     D = data["D"]
     f = data["f"]
@@ -26,7 +27,7 @@ def test_generated(ivim_algorithm, ivim_data, SNR, rtol, atol, fit_count, rician
     fit = OsipiBase(algorithm=ivim_algorithm)
     # here is a prior
     if use_prior and hasattr(fit, "accepts_priors") and fit.accepts_priors:
-        prior = [np.random.normal(D, D/3, 10), np.random.normal(f, f/3, 10), np.random.normal(Dp, Dp/3, 10), np.random.normal(1, 1/3, 10)]
+        prior = [rng.normal(D, D/3, 10), rng.normal(f, f/3, 10), rng.normal(Dp, Dp/3, 10), rng.normal(1, 1/3, 10)]
         # prior = [np.repeat(D, 10)+np.random.normal(0,D/3,np.shape(np.repeat(D, 10))), np.repeat(f, 10)+np.random.normal(0,f/3,np.shape(np.repeat(D, 10))), np.repeat(Dp, 10)+np.random.normal(0,Dp/3,np.shape(np.repeat(D, 10))),np.repeat(np.ones_like(Dp), 10)+np.random.normal(0,1/3,np.shape(np.repeat(D, 10)))]  # D, f, D*
         fit.initialize(prior_in=prior)
     time_delta = datetime.timedelta()
@@ -38,11 +39,13 @@ def test_generated(ivim_algorithm, ivim_data, SNR, rtol, atol, fit_count, rician
         start_time = datetime.datetime.now()
         [f_fit, Dp_fit, D_fit] = fit.osipi_fit(signal, bvals) #, prior_in=prior
         time_delta += datetime.datetime.now() - start_time
-        if save_file:
-            save_results(save_file, ivim_algorithm, name, SNR, idx, [f, Dp, D], [f_fit, Dp_fit, D_fit])
+        if save_file is not None:
+            save_file.writerow([ivim_algorithm, name, SNR, idx, f, Dp, D, f_fit, Dp_fit, D_fit, *signal])
+            # save_results(save_file, ivim_algorithm, name, SNR, idx, [f, Dp, D], [f_fit, Dp_fit, D_fit])
         npt.assert_allclose([f, Dp, D], [f_fit, Dp_fit, D_fit], rtol, atol)
-    if save_duration_file:
-        save_duration(save_duration_file, ivim_algorithm, name, SNR, time_delta, fit_count)
+    if save_duration_file is not None:
+        save_duration_file.writerow([ivim_algorithm, name, SNR, time_delta/datetime.timedelta(microseconds=1), fit_count])
+        # save_duration(save_duration_file, ivim_algorithm, name, SNR, time_delta, fit_count)
 
 
 def save_results(filename, algorithm, name, SNR, idx, truth, fit):
