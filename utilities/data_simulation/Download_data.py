2a8de37a32d99173d218af0fdb1eaf80bf025dca
Oliver Gurney-Champion <oliverchampion@gmail.com>
2024-02-28 12:33:20 +0100

    fixed the waiting

--- utilities/data_simulation/Download_data.py
+++ utilities/data_simulation/Download_data.py
@@ -3,8 +3,18 @@ import os
 import subprocess
 import time
 
+
+def unzip_file(zip_file_path, extracted_folder_path):
+    # Open the zip file
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        # Extract each file one by one
+        for file_info in zip_ref.infolist():
+            zip_ref.extract(file_info, extracted_folder_path)
+
+
 def download_data(force=False):
     # Check if the folder exists, and create it if not
+    curdir=os.getcwd()
     base_folder = os.path.abspath(os.path.dirname(__file__))
     base_folder = os.path.split(os.path.split(base_folder)[0])[0]
     if not os.path.exists(os.path.join(base_folder,'download')):
@@ -17,13 +27,13 @@ def download_data(force=False):
         time.sleep(1)
     # Open the zip file
     if force or not os.path.exists('Data'):
-        with zipfile.ZipFile('OSIPI_TF24_data_phantoms.zip', 'r') as zip_ref:
-            # Extract all the contents to the destination folder
-            zip_ref.extractall('.')
-        while not os.path.exists('Data'):
-            time.sleep(10)
-        while not os.path.exists('Phantoms'):
-            time.sleep(10)
-        while not os.path.exists('Utilities'):
-            time.sleep(10)
+        # Unzip the file
+        unzip_file('OSIPI_TF24_data_phantoms.zip', '.')
+        # Wait for the extraction to complete by checking for the existence of any file
+        # Wait for the extraction to complete by checking for expected subdirectories
+        expected_subdirectories = [os.path.join('Utilities','DRO.npy'),os.path.join('Data','abdomen.nii.gz'), os.path.join('Data','brain.nii.gz'), os.path.join('Phantoms','brain','data','ballistic_snr200.nii.gz'), os.path.join('Phantoms','brain','data','diffusive_snr200.nii.gz'), os.path.join('Phantoms','XCAT_MAT_RESP','XCAT5D_RP_1_CP_1.mat'), os.path.join('Phantoms','XCAT_MAT_RESP','XCAT5D_RP_9_CP_1.mat'), os.path.join('Phantoms','XCAT_MAT_RESP','XCAT5D_RP_20_CP_1.mat')]  # Add the expected subdirectories
+        while not all(
+                os.path.isfile(subdir) for subdir in expected_subdirectories):
+            time.sleep(1)  # Wait for 1 second
         time.sleep(10)
+    os.chdir(curdir)
