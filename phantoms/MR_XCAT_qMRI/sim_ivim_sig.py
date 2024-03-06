667cbf5bb344cc5b2ecac8941cefbc67b2cc90b7
Unique-Usman <usmanakinyemi202@gmail.com>
2024-03-06 18:38:19 +0530

    Updated the generic_0.json and corrected the sim_ivim_sig.py

--- phantoms/MR_XCAT_qMRI/sim_ivim_sig.py
+++ phantoms/MR_XCAT_qMRI/sim_ivim_sig.py
@@ -105,7 +105,7 @@ def contrast_curve_calc():
     D[36] = 3e-3  # 36 artery
     D[37] = 3e-3  # 37 vein
     D[40] = 1.31e-3  # 40 asc lower intestine : Hai-Jing et al. doi: 10.1097/RCT.0000000000000926
-    D[41] = 1.31e-3  # 41 trans lower# If conversion fails, try splitting the input as a lis intestine : Hai-Jing et al. doi: 10.1097/RCT.0000000000000926
+    D[41] = 1.31e-3  # 41 trans lower intestine : Hai-Jing et al. doi: 10.1097/RCT.0000000000000926
     D[42] = 1.31e-3  # 42 desc lower intestine : Hai-Jing et al. doi: 10.1097/RCT.0000000000000926
     D[43] = 1.31e-3  # 43 small intestine : Hai-Jing et al. doi: 10.1097/RCT.0000000000000926
     D[50] = 3e-3  # 50 pericardium
@@ -395,7 +395,7 @@ if __name__ == '__main__':
     parser.add_argument("-b", "--bvalue", type=float,
                         nargs="+",
                         help="B values (list of of numbers)")
-    parser.add_argument("-bf", "--bvalues-file", metavar="FILE", type=parse_bvalues_file,
+    parser.add_argument("-f", "--bvalues-file", metavar="FILE", type=parse_bvalues_file,
                     help='JSON file containing the b-values')
     parser.add_argument("-n", "--noise", type=float, default=0.0005, help="Noise")
     parser.add_argument("-m", "--motion", action="store_true", help="Motion flag")
