7294bdeb192cd925d98889f1c9d79d47062196c1
Usman Akinyemi <usmanakinyemi202@gmail.com>
2024-09-26 07:38:40 +0000

    Generated the testing images during docker building and added confirmation to ensure files are being generated

--- Docker/generate_signal_docker_test.py
+++ Docker/generate_signal_docker_test.py
@@ -1,17 +1,24 @@
 import numpy as np
 import nibabel as nib
 from utilities.data_simulation.GenerateData import GenerateData
+from WrapImage.nifti_wrapper import save_nifti_file
 
-def save_nii(data, filename='ivim_image.nii.gz'):
-    """
-    Save the data as a NIfTI file (.nii.gz)
-    """
-    nii_img = nib.Nifti1Image(data, affine=np.eye(4))
-    nib.save(nii_img, filename)
+
+def save_bval_bvec(filename, values):
+    if filename.endswith('.bval'):
+        # Convert list to a space-separated string for bval
+        values_string = ' '.join(map(str, values))
+    elif filename.endswith('.bvec'):
+        # Convert 2D list to a line-separated, space-separated string for bvec
+        values_string = '\n'.join(' '.join(map(str, row)) for row in values)
+    else:
+        raise ValueError("Unsupported file extension. Use '.bval' or '.bvec'.")
+
+    with open(filename, 'w') as file:
+        file.write(values_string)
 
 # Set random seed for reproducibility
 np.random.seed(42)
-
 # Create GenerateData instance
 gd = GenerateData()
 
@@ -25,7 +32,11 @@ bvals = np.array([0, 50, 100, 500, 1000])
 bvals_reshaped = np.broadcast_to(bvals, shape)
 
 # Generate IVIM signal
-images = gd.ivim_signal(D_in, Dp_in, f_in, S0, bvals_reshaped)
+signals = gd.ivim_signal(D_in, Dp_in, f_in, S0, bvals_reshaped)
 
 # Save the generated image as a NIfTI file
-save_nii(images)
+save_nifti_file(signals, "ivim_image.nii.gz")
+# Save the bval in a file
+save_bval_bvec("ivim_image.bval", [0, 50, 100, 500, 1000])
+# Save the bvec value 
+save_bval_bvec("ivim_signal.bvec", [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
