1fa952cd30dd6ac7bfedbfa4dd59e0eff6db4403
Usman Akinyemi <usmanakinyemi202@gmail.com>
2024-09-26 00:42:45 +0000

    Fixed the float not subscriptable error

--- utilities/data_simulation/GenerateData.py
+++ utilities/data_simulation/GenerateData.py
@@ -75,7 +75,7 @@ class GenerateData:
         assert len(D) == len(F), 'D and F must be the same length'
         signal = self._op.zeros_like(bvalues)
         for [d, f] in zip(D, F):
-            signal += f * self.exponential_signal(d[0], bvalues)
+            signal += f * self.exponential_signal(d, bvalues)
         signal *= S0
         return signal
 
