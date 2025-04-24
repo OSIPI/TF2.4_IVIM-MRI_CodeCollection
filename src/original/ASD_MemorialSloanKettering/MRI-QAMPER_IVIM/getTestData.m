a16b99a066a50b92d58d8c0410d34f170535ee1e
locastre <34068528+locastre@users.noreply.github.com>
2025-04-24 14:39:38 -0400

    Update getTestData.m to retrieve and use OSIPI Zenodo data

--- src/original/ASD_MemorialSloanKettering/MRI-QAMPER_IVIM/getTestData.m
+++ src/original/ASD_MemorialSloanKettering/MRI-QAMPER_IVIM/getTestData.m
@@ -1,8 +1,16 @@
-function [img_nii,bval_file,roi_nii,save_folder] = getTestData
+function [img_nii,bval_file,roi_nii] = getTestData
 
 [d,~,~] = fileparts(which('getTestData'));
+test_data_folder = fullfile(d,'test_data');
+if ~exist(test_data_folder,'dir')
+    mkdir(test_data_folder);
+end
 
-img_nii = fullfile(d,'test_data','702-HN401D-D2019_10_08.nii.gz');
-bval_file = fullfile(d,'test_data','702-HN401D-D2019_10_08.bval');
-roi_nii = fullfile(d,'test_data','702-HN401D-D2019_10_08_BD_REDO_SV.nii.gz');
-save_folder = fullfile(d,'test_data');
+zenodo_url = 'https://zenodo.org/records/14605039/files/OSIPI_TF24_data_phantoms.zip';
+save_zip = fullfile(test_data_folder,'OSIPI_TF24_data_phantoms.zip');
+websave(save_zip,zenodo_url);
+unzip(save_zip, test_data_folder);
+
+img_nii = fulllfile(test_data_folder,'Data','brain.nii.gz');
+bval_file = fulllfile(test_data_folder,'Data','brain.bval');
+roi_nii = fulllfile(test_data_folder,'Data','brain_mask_gray_matter.nii.gz');
