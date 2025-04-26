4c9aaff8f456ecbb4e286d723c0f53d814ef3497
locastre <34068528+locastre@users.noreply.github.com>
2025-04-26 15:32:54 -0400

    Update demo_QAMPER_IVIM.m

--- src/original/ASD_MemorialSloanKettering/MRI-QAMPER_IVIM/demo_QAMPER_IVIM.m
+++ src/original/ASD_MemorialSloanKettering/MRI-QAMPER_IVIM/demo_QAMPER_IVIM.m
@@ -10,15 +10,21 @@
 %       roi_nii: single-volume mask ROI image (NIfTI)
 
 
-
-
-qamper_path = 'path:\to\MRI-QAMPER_IVIM';
+qamper_path = 'path:\to\MRI-QAMPER_IVIM'; %path where the MRI-QAMPER folder is located
 addpath(genpath(qamper_path));
 
-img_nii = 'dwi.nii';
-bval_file = fullfile(qamper_path,'test_data','702-HN401D-D2019_10_08.bval');
-roi_nii = fullfile(qamper_path,'test_data','702-HN401D-D2019_10_08_BD_REDO_SV.nii.gz');
+% Uncomment below and substitute the names of your files
+% img_nii = '702-D2019_10_08.nii';
+% bval_file = fullfile(qamper_path,'test_data','702-D2019_10_08.bval');
+% roi_nii = fullfile(qamper_path,'test_data','702-D2019_10_08_ROI.nii.gz');
 
-save_folder = fullfile(qamper_path,'test_data');
+% Optional function below: download the OSIPI test data from Zenodo
+[img_nii, bval_file, roi_nii] = getTestData;
 
+save_folder = fullfile(qamper_path,'output_files'); %change this location to where you want the output files to be saved
+if ~exist(save_folder,'dir')
+    mkdir(save_folder);
+end
+
+% Run the analysis
 batchResultsFolder = run_QAMPER_IVIM(img_nii,bval_file,roi_nii,save_folder);
