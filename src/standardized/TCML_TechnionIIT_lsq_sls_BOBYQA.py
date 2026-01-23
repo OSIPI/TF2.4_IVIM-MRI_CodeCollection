7bf6d2cdd50b2c227738b23a97edc131d7f1941e
IvanARashid <ivan.rashid@proton.me>
2026-01-23 12:40:02 +0100

    testing a fix by turning bounds into floats for the failing algorithms

--- src/standardized/TCML_TechnionIIT_lsq_sls_BOBYQA.py
+++ src/standardized/TCML_TechnionIIT_lsq_sls_BOBYQA.py
@@ -69,8 +69,8 @@ class TCML_TechnionIIT_lsq_sls_BOBYQA(OsipiBase):
         """
         signals[signals<0]=0
         bvalues=np.array(bvalues)
-        bounds = ([self.bounds["D"][0], self.bounds["Dp"][0], self.bounds["f"][0], self.bounds["S0"][0]],
-                       [self.bounds["D"][1], self.bounds["Dp"][1], self.bounds["f"][1], self.bounds["S0"][1]])
+        bounds = ([float(self.bounds["D"][0]), float(self.bounds["Dp"][0]), float(self.bounds["f"][0]), float(self.bounds["S0"][0])],
+                       [float(self.bounds["D"][1]), float(self.bounds["Dp"][1]), float(self.bounds["f"][1]), float(self.bounds["S0"][1])])
 
         fit_results = self.fit_least_squares(np.array(signals)[:,np.newaxis],bvalues, bounds,min_bval_high=self.thresholds)
 
